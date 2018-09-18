9cc3e84f24de834d1cbcd3780226dcd6f3831dd6
Gilbert Torchon <gilbert@noukod.com>
2018-09-18 19:36:57 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,13 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    array.each do |element|
+      yield(element)
+    end
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
+names = ["Tim","Tom","Jim"]
+hello_t(names) {|name| puts name}
